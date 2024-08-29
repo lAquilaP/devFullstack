23cd20b716534dce76cb480599054e5794f12faa
lAquilaP <161894977+lAquilaP@users.noreply.github.com>
2024-08-28 21:56:03 -0300

    Add files via upload

---
+++ component/breadcrumb.php
@@ -0,0 +1,18 @@
+<section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1><?= $breadcrumb_title ?></h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <?php for($i = 0; $i < count($breadcrumb_path) - 1; $i++) : ?>
+                  <li class="breadcrumb-item"><a href="#"><?= $breadcrumb_path[$i] ?></a></li>
+              <?php endfor ?>
+              <li class="breadcrumb-item active"><?= 
+              $breadcrumb_path[count($breadcrumb_path) - 1] ?></li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
